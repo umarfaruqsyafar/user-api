4ee8a7106b1ac78dfe81666ac0cb4b58a1cdd397
umarfaruqsyafar <ufaruqsy@gmail.com>
2025-04-07 03:30:20 +0700

    feat: create User model with UUID and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,24 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
+     * UUID sebagai primary key (string, non-incrementing)
+     */
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    /**
+     * Kolom yang boleh diisi (mass assignable)
      */
     protected $fillable = [
         'name',
@@ -24,22 +28,16 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
+     * Generate UUID saat model dibuat
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected static function boot()
+    {
+        parent::boot();
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = (string) Str::uuid();
+            }
+        });
+    }
 }
