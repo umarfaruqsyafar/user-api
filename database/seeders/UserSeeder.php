03ff3f1e15dbf6ed309d9e9038be100e496d24cf
umarfaruqsyafar <ufaruqsy@gmail.com>
2025-04-07 04:22:20 +0700

    revert: remove Docker setup and restore local DB config

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::factory()->count(10)->create();
+    }
+}
